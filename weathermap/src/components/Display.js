82d5b48ad210a65531868c99b1dd3aa727fcf2b9
Reuben T Allik <reubenallik@gmail.com>
2021-04-04 23:11:41 -0400

    added maptiler satellite view

--- weathermap/src/components/Display.js
+++ weathermap/src/components/Display.js
@@ -1,12 +1,39 @@
 import React, {useState} from 'react';
-import L from 'leaflet';
+import L, { Layer } from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup, useMap, LayersControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const Display = () => {
-    const API_KEY = process.env.REACT_APP_API_KEY;
-    const layer = 'clouds_new';
-    const owm_url = `https://tile.openweathermap.org/map/${layer}/{z}/{x}/{y}.png?appid=${API_KEY}`
+
+    //API KEY
+    const OWM_KEY = process.env.REACT_APP_OWM_KEY;
+    const M_KEY = process.env.REACT_APP_M_KEY;
+
+    //Open Street Maps
+    const osm_attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+    const osm_source = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    
+    //OpenWeather
+    const owm_attribution = '<a href="https://openweathermap.org/">OpenWeatherMap</a>';
+    const owm_url = {
+        a: 'https://tile.openweathermap.org/map/',
+        b:`/{z}/{x}/{y}.png?appid=${OWM_KEY}`
+    };
+    
+    //Maptiler 
+    const m_attr = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors &vert; <a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a>'; 
+    const m_url = `https://api.maptiler.com/tiles/satellite/{z}/{x}/{y}.jpg?key=${M_KEY}`
+
+    //OWM Layer values
+    const layers = {
+        clouds: 'clouds_new',
+        precip: 'precipitation_new',
+        pressure: 'pressure_new',
+        wind: 'wind_new',
+        temp: 'temp_new'
+    };
+
+    //Initial map center
     const [state, setState] = useState({
         lat: 42.3601,
         long: -71.0589,
@@ -14,39 +41,44 @@ const Display = () => {
         scrollZoom: false
     });
 
-        
+    //State destructuring    
     const { zoom, lat, long, scrollZoom } = state;
+    
+    //Converting lat, long object to array
     const position = [lat, long];
     
+    //Testing useMap
     const DisplayMap = () => {
         const map = useMap();
         console.log('map', map.getCenter())
         return null;
     }
     
-    // console.log(<TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />);
-    
     return (
 
         <MapContainer className='map' center={position} zoom={zoom} scrollWheelZoom={scrollZoom}>
-            <LayersControl position='topright'>
-                <LayersControl.BaseLayer
-                    checked
-                    name="OpenStreetMap"
-                >
-                    <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                </LayersControl.BaseLayer> 
-                <LayersControl.Overlay
-                    name=""
-                >
-                <TileLayer
-                    attribution='Data from <a href="https://openweathermap.org/">OpenWeather</a>'
-                    url={owm_url}
-                />
+            <LayersControl position='topright' onChange={() => setState(...state)}>
+                <LayersControl.BaseLayer checked name="Open Street Maps">
+                    <TileLayer attribution={osm_attribution} url={osm_source}/>
+                </LayersControl.BaseLayer>
+                <LayersControl.BaseLayer name="Maptiler Satellite">
+                    <TileLayer attribution={m_attr} url={m_url}/>
+                </LayersControl.BaseLayer>
+
+                <LayersControl.Overlay name='Clouds' >
+                    <TileLayer attribution={owm_attribution} url={owm_url.a + layers.clouds + owm_url.b}/>
+                </LayersControl.Overlay>
+                <LayersControl.Overlay name='Pricipitation'>
+                    <TileLayer attribution={owm_attribution} url={owm_url.a + layers.precip + owm_url.b}/>
+                </LayersControl.Overlay>
+                <LayersControl.Overlay name='Pressure'>
+                    <TileLayer attribution={owm_attribution} url={owm_url.a + layers.pressure + owm_url.b}/>
+                </LayersControl.Overlay>
+                <LayersControl.Overlay name='Wind'>
+                    <TileLayer attribution={owm_attribution} url={owm_url.a + layers.wind + owm_url.b}/>
+                </LayersControl.Overlay>
+                <LayersControl.Overlay name='Temp'>
+                    <TileLayer attribution={owm_attribution} url={owm_url.a + layers.temp + owm_url.b}/>
                 </LayersControl.Overlay>
                 
                 {/* <Marker position={position}>
